cc4c6242a1b7898a78a10e6ebec956da15f76ad3
DrBooly <99161410+DrBooly-491@users.noreply.github.com>
2026-04-21 03:10:54 +1000

    Update fxmanifest.lua
    thought i was going crazy when it said 2.0.0 thanks TehGuardian

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,11 @@ fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
 
-version '1.0.0'
+version '1.0.1'
 repository 'https://github.com/Mythic-Framework/mythic-damage'
 
 client_script "@mythic-base/components/cl_error.lua"
 client_script "@mythic-pwnzor/client/check.lua"
-
-version '2.0.1'
 
 client_scripts {
 	'shared/**/*.lua',
